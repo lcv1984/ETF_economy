95faf2717cef9a043153728fcfa9e753fe8e76f8
Luis C. Vargas <lcv1984@gmail.com>
2015-04-12 19:12:56 -0400

    filter input json file to rows with data on requested country
    country is now set to a default value, next need to allow user to
    request a new country to plot.

--- d3/correlation.js
+++ d3/correlation.js
@@ -2,43 +2,63 @@
 var margin = {top: 30, right: 30, bottom: 30, left: 30}
 var width = 300 - margin.left - margin.right;
 var height= 300 - margin.top - margin.bottom;
-var barPadding = 0.5; 
-    
-d3.csv("econvar_similarity.csv", function(data) {
-    dataset = data;
-    console.log(dataset);
-    nvars = d3.max(dataset, function(d) { return parseInt(d.x); }) + 1;
+var barPadding = 0.5;
+var simvals;
+var series;
+var subsimvals;
+var country = "Peru"
+
+d3.json("../data/econvar_similarity.json", function(error1, data1) {
+  d3.json("../data/econvar_timeseries.json", function(error2, data2) {
+    simvals = data1;
+    series  = data2;
+    console.log(error1);
+    console.log(error2);
+
+    function filterByCountry(obj) {
+      if (obj.country == country) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    subsimvals = simvals.filter(filterByCountry);
+
+    console.log(subsimvals);
+
+    nvars = d3.max(simvals, function(d) { return parseInt(d.index1); }) + 1;
     var dx = width / nvars;
     var dy = width / nvars;
 
     var xScale = d3.scale.linear()
-        .domain(d3.extent(dataset, function(d) { return parseInt(d.x); }))
+        .domain(d3.extent(simvals, function(d) { return parseInt(d.index1); }))
         .range([margin.left,margin.left+width]);
 
     var yScale = d3.scale.linear()
-        .domain(d3.extent(dataset, function(d) { return parseInt(d.y); }))
+        .domain(d3.extent(simvals, function(d) { return parseInt(d.index1); }))
         .range([margin.top,margin.top+height]);
 
     var zScale = d3.scale.linear()
-        .domain(d3.extent(dataset, function(d) { return parseInt(d.simscore); }))
+        .domain(d3.extent(simvals, function(d) { return parseInt(d.simscore); }))
         .range(["steelblue","white"]);
-    
+
     xScale.domain([xScale.domain()[0],xScale.domain()[1] + 1]);
     yScale.domain([yScale.domain()[0],yScale.domain()[1] + 1]);
 
     var svg = d3.select("div #heatmap").append("svg")
-        .attr("width",width + margin.top + margin.bottom) 
+        .attr("width",width + margin.top + margin.bottom)
         .attr("height",height + margin.left + margin.right)
-        
+
     svg.append("g")
         .attr("transform","translate(" + margin.left.toString() + "," + margin.top.toString() + ")");
 
     svg.selectAll(".tile")
-        .data(dataset)
+        .data(simvals)
         .enter().append("rect")
         .attr("class", "tile")
-        .attr("x", function(d) { return xScale(d.x); })
-        .attr("y", function(d) { return yScale(d.y); })
+        .attr("x", function(d) { return xScale(d.index1); })
+        .attr("y", function(d) { return yScale(d.index2); })
         .attr("width", dx)
         .attr("height", dy)
         .style("fill", function(d) {return zScale(d.simscore); });
@@ -56,16 +76,21 @@ d3.csv("econvar_similarity.csv", function(data) {
         .attr("x", function(d) { return width/2.; })
         .attr("y", function(d) { return 30; })
         .text("Economic Variable");
-    
+
     //highlight box when mouseover
     svg.selectAll(".tile")
         .on("mouseover", function(d) {
             d3.select(this)
-            .style("fill", function(d) { return "blue"; });
+              .style("fill", function(d) { return "blue"; });
+            var svg2 = d3.select("div #timeseries").append('svg')
+              .attr("width",width + margin.top + margin.bottom)
+              .attr("height",height + margin.left + margin.right)
+
         })
         .on("mouseout", function(d) {
             d3.select(this)
             .style("fill", function(d) { return zScale(d.simscore); });
         });
-       
- });
+
+  });
+});
